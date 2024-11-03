7b7782da114be8527ccef971cf9defe8db1d847a
Rick D <dev.bites.uk@gmail.com>
2024-11-03 21:26:45 +0900

    add: .gitignore

---
+++ tree/tree.c
@@ -0,0 +1,7 @@
+#include "tree.h"
+
+int main() {
+  printf("Hello World\n");
+
+  return 0;
+}
---
+++ tree/tree.h
@@ -0,0 +1,8 @@
+#define _GNU_SOURCE
+#include <assert.h>
+#include <errno.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
+
