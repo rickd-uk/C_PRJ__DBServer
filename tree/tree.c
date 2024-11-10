dcd66e2252e2c0d4815e179682b19195ce4ee1f0
Rick D <dev.bites.uk@gmail.com>
2024-11-10 18:17:27 +0900

    remove: tree from tracking

--- tree/tree.c
+++
@@ -1,7 +0,0 @@
-#include "tree.h"
-
-int main() {
-  printf("Hello World\n");
-
-  return 0;
-}
--- tree/tree.h
+++
@@ -1,8 +0,0 @@
-#define _GNU_SOURCE
-#include <assert.h>
-#include <errno.h>
-#include <stdio.h>
-#include <stdlib.h>
-#include <string.h>
-#include <unistd.h>
-
